0abd1fbba4b9e3114ceb5ca495c80f6a4c5d5db5
pratik kagale <pratikkagale3333@gmail.com>
2022-09-20 08:39:46 +0530

    C Program to input a character from user and check whether it is vowel or consonant

--- C-training/practical/practice/Day 15/ifstate.c
+++ C-training/practical/practice/Day 15/ifstate.c
@@ -7,11 +7,11 @@ int main(int argc, char const *argv[])
 
     if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
     {
-        printf("vowel");
+        printf("%c is a vowel.",c);
     }
     else
     {
-        printf("Consonant");
+        printf("%c is a Consonant.",c);
     }
     
     return 0;
