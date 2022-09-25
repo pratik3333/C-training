0f389dec100d946c870bfbba88cab22b77a68907
pratik kagale <pratikkagale3333@gmail.com>
2022-09-25 18:46:27 +0530

    alphabet is vowel or consonant using switch case.

---
+++ C-training/practical/practice/Example/vowelorconso.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    char ch;
+    printf("Enter the character: ");
+    scanf("%c",&ch);
+
+    switch (ch)
+    {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U':
+    printf("Vowel");
+
+        break;
+    
+    default:
+    printf("Consonant");
+        break;
+    }
+    return 0;
+}
