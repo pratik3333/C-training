1dcc670b9e50c82c0268315bbccbcce25591f8b9
pratik kagale <pratikkagale3333@gmail.com>
2022-09-27 09:20:04 +0530

    numbers from 100 to 1

---
+++ C-training/practical/practice/Example/Loop/reversnumber.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i;
+    i=100;
+    while (i>=1)
+    {
+     
+        printf("%d\n",i);
+
+        --i;
+    }
+    
+    return 0;
+}
