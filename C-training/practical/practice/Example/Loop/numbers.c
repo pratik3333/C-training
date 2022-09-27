034b508ed752d2b2cea3f584dd042cb72c1c1eb8
pratik kagale <pratikkagale3333@gmail.com>
2022-09-27 09:01:31 +0530

    numbers from 1 to 100 using while loop

---
+++ C-training/practical/practice/Example/Loop/numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i;
+    i=1;
+    while (i<=100)
+    {
+        printf("%d\n",i);
+
+        i++;
+    }
+    
+    return 0;
+}
