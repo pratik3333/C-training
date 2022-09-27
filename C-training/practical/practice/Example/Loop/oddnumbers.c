c9ddc3aedac72e86c18ea4c25b6ffbda19d0be2d
pratik kagale <pratikkagale3333@gmail.com>
2022-09-27 08:57:53 +0530

    odd numbers between 1-10 using while loop

--- C-training/practical/practice/Example/Loop/oddnumbers.c
+++ C-training/practical/practice/Example/Loop/oddnumbers.c
@@ -3,17 +3,16 @@ int main(int argc, char const *argv[])
 {
     int i;
     i=1;
-
     while (i<=10)
     {
-        if (i%2==0)
+        if (i%2!=0)
         {
             printf("%d\n",i);
+
         }
         
         i++;
     }
     
-
     return 0;
 }
