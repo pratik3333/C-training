6077f09f643ef34167723eef0b2ceddc479ea244
pratik kagale <pratikkagale3333@gmail.com>
2022-09-27 08:45:25 +0530

    While loop

---
+++ C-training/practical/practice/Example/Loop/1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i;
+    i=1;
+    while (i<=5)
+    {
+        printf("Good Morning\n");
+        i++;
+    }
+    
+    return 0;
+}
