2bc38942fe0b9e6ba8913f15cbc3c320bae97520
pratik kagale <pratikkagale3333@gmail.com>
2022-09-27 08:51:35 +0530

    even numbers between 1-10 using while loop

---
+++ C-training/practical/practice/Example/Loop/evennumber.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i;
+    i=1;
+
+    while (i<=10)
+    {
+        if (i%2!=0)
+        {
+            printf("%d\n",i);
+        }
+        
+        i++;
+    }
+    
+
+    return 0;
+}
