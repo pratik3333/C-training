6bdabb21478cb826fc39c9e55d06389a81019146
pratik kagale <pratikkagale3333@gmail.com>
2022-09-28 09:28:50 +0530

    write a program input the two values consider first as base and second as index and calculate its power

--- C-training/practical/practice/Example/Loop/while/power.c
+++ C-training/practical/practice/Example/Loop/while/power.c
@@ -1,1 +1,20 @@
 // write a program input the two values consider first as base and second as index and calculate its power
+
+
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int base,index,p=1;
+    printf("Enter the base and Index: ");
+    scanf("%d%d",&base,&index);
+
+    while (index!=0)
+    {
+        p=p*base;
+        index=index-1;
+
+    }
+    printf("Power is %d\n",p);
+
+    return 0;
+}
