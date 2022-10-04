9e992d82415eb6674877123346f40697d49020cb
pratik kagale <pratikkagale3333@gmail.com>
2022-10-04 09:52:29 +0530

    pattern printing

---
+++ C-training/practical/practice/Example/Loop/For/patter.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    
+    for (int  i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <=5; j++)
+        {
+            if (i>=j)
+            {
+                printf("%d",j);
+            }
+            
+        }
+        printf("\n");
+    }
+    
+
+    return 0;
+}
