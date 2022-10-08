c84c6263c471c346065659b470e46b3d1db4599d
pratik kagale <pratikkagale3333@gmail.com>
2022-10-08 09:05:29 +0530

    printing pattern

---
+++ C-training/practical/practice/Example/Loop/For/p2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i,j;
+    for ( i = 1; i <=5; i++)
+    {
+        for ( j = 1; j <= 5; j++)
+        {
+            if (j==1 || i==5 || j==6-i )
+            {
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+            
+        }
+        
+        printf("\n");
+    }
+    
+    return 0;
+}
+
+
+
+// output:-
+
+
+// *       *
+// *     *
+// *   *
+// * *
+// * * * * *
+
+
+
