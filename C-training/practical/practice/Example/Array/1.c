5be85cd0b89df7aac6edc8cf72dcd35a68494294
pratik kagale <pratikkagale3333@gmail.com>
2022-10-10 09:03:48 +0530

    something went wrong

--- C-training/practical/practice/Example/Array/1.c
+++ C-training/practical/practice/Example/Array/1.c
@@ -1,9 +1,11 @@
+// write a program input the 5 value in array and find the maximum value in array
+
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
     int a[5],i,max;
 
-    printf("Enter the five values in array:\n ");
+    printf("Enter the five values in array:\n");
     for ( i = 0; i <5; i++)
     {
         scanf("%d",&a[i]);
--- C-training/practical/practice/Example/Loop/For/p6.c
+++ C-training/practical/practice/Example/Loop/For/p6.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i,j;
+    for ( i =1; i <6; i++)
+    {
+        for ( j = 1; j < 6; j++)
+        {
+            if (j>=6-i)
+            {
+                printf(" ");
+            }else
+            {
+                printf("%c",j);
+            }
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
