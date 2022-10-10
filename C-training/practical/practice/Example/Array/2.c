17e42494c9c56931ceed62ac7164bc4509333d1e
pratik kagale <pratikkagale3333@gmail.com>
2022-10-10 09:31:17 +0530

    input five values in array and calculate it's sum

--- C-training/practical/practice/Example/Array/2.c
+++ C-training/practical/practice/Example/Array/2.c
@@ -1,14 +1,18 @@
+// Write a program to store 5 values in array and search a particular value from array.
+// if value found then program should print message value found if value not found then program 
+// should print message value not found.
+
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
     int a[5],i,value;
-    printf("Enter the values in array");
+    printf("Enter the values in array:\n");
     for ( i = 0; i <5; i++)
     {
         scanf("%d",&a[i]);
     }
 
-    printf("Enter the value for search");
+    printf("Enter the value for search: ");
     scanf("%d",&value);
     int m=0;
     for ( i = 0; i <5; i++)
---
+++ C-training/practical/practice/Example/Array/sum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a[5],i,sum=0;
+    printf("Enter the five values in array: ");
+    for ( i = 0; i <5; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("Display the all values of array\n");
+    for ( i = 0; i <5; i++)
+    {
+        printf("a[%d]---->%d\n",i,a[i]);
+
+        sum=sum+a[i];
+
+    }
+    printf("Sum of all elements is %d\n",sum);
+    
+    return 0;
+}
