d8c00f3d0a308d32ad0b25b5cedc3b9a4102b871
pratik kagale <pratikkagale3333@gmail.com>
2022-10-12 09:42:42 +0530

    inserted 10 elements into array and calculated product of all elements into array

---
+++ C-training/practical/practice/Example/Array/product_of_array.c
@@ -0,0 +1,21 @@
+// W.A.P. to insert 10 elements into array and calculate product of all elements into array.
+
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i,a[10],mul=1;
+
+    printf("Enter the 10 elements of array:\n");
+    for ( i = 0; i <10; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    for ( i = 0; i < 10; i++)
+    {
+        mul=mul*a[i];
+    }
+    
+    printf("Multiplication of all elements of array:%d",mul);
+    return 0;
+}
