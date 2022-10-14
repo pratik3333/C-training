3d95c4b8d0efd0989e3c29d65e77b9beda8a2a74
pratik kagale <pratikkagale3333@gmail.com>
2022-10-14 17:50:42 +0530

    C

---
+++ C-training/test 1/series.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i,n;
+    int t1=0,t2=1;
+
+    int nextterm=t1+t2;
+
+    // taking input from user
+    printf("Enter the number:\n");
+    scanf("%d",&n);
+    
+
+    // printing loop up to input number
+    for ( i = 3; i <n; i++)
+    {
+        t1=t2;
+        t2=nextterm;
+        nextterm=t1+t2;
+
+        
+    }
+    printf("%d",temp);
+    
+    return 0;
+}
