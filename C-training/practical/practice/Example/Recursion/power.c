7b2384c117b3451dc06cc82cdfb3f6904c0f5e62
pratik kagale <pratikkagale3333@gmail.com>
2022-11-06 23:52:58 +0530

    calculated power using recursion

--- C-training/practical/practice/Example/Recursion/power.c
+++ C-training/practical/practice/Example/Recursion/power.c
@@ -1,25 +1,35 @@
-// write a program input the teo values consider first as base and second as index
-//  and calculate its power using recursion.
+// // write a program input the teo values consider first as base and second as index
+// //  and calculate its power using recursion.
+
+// #include <stdio.h>
+// int p = 1;
+
+// int power(int base, int index)
+// {
+//     if (index != 0)
+//     {
+//         p = p * base;
+//         power(base, --index);
+//     }
+//     return p;
+// }
+// int main(int argc, char const *argv[])
+// {
+//     int base, index;
+//     printf("Enter the base and index value: ");
+//     scanf("%d%d", &base,&index);
+//     int result = power(base, index);
+//     printf("\n power is %d\n", result);
+    
+//     return 0;
+// }
 
 #include <stdio.h>
-int p = 1;
-
-int power(int base, int index)
-{
-    if (index != 0)
-    {
-        p = p * base;
-        power(base, --index);
-    }
-    return p;
-}
 int main(int argc, char const *argv[])
 {
-    int base, index;
-    printf("Enter the base and index value: ");
-    scanf("%d%d", &base,&index);
-    int result = power(base, index);
-    printf("\n power is %d\n", result);
-    
+    char ch[9];
+    printf("Enter \n");
+    gets(ch);
+    printf("\nName is %s\n",ch);
     return 0;
 }
