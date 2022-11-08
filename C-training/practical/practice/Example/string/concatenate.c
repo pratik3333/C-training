fda68b152fdc91dfd49e2e8c659642c8c5715c3f
pratik kagale <pratikkagale3333@gmail.com>
2022-11-08 12:11:08 +0530

    printed table using recursion

---
+++ C-training/practical/practice/Example/string/concatenate.c
@@ -0,0 +1,18 @@
+// Write a C program to concatenate two strings
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+    char first[100];
+    char second[100];
+    printf("Enter the first string: ");
+    gets(first);
+    printf("Enter the second string: ");
+    gets(second);
+
+    
+    return 0;
+}
---
+++ C-training/practical/practice/Example/string/palindrome.c
@@ -0,0 +1,14 @@
+// Write a C program to check wheather the given string is palindrome or not
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int 
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
