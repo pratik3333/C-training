d593d70404a1e40a4338dc624fb3d43a5fc42017
pratik kagale <pratikkagale3333@gmail.com>
2023-02-23 17:19:48 +0530

    all even numbers between 0 to 100

---
+++ Feb-c/while_loop/even_number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int i=0;
+
+    while (i<=100)
+    {
+        if (i%2==0)
+        {
+            printf("%d\n",i);
+
+        }
+        i++;
+    }
+    
+}
