a4501952dde4ade77cc4a1ad368879ccef923f6e
pratik kagale <pratikkagale3333@gmail.com>
2023-02-25 11:09:25 +0530

    Write a C program to find maximum between two numbers using switch case.

---
+++ Feb-c/switch/switch_case/max_between_two.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1,num2;
+    printf("Enter the first number: ");
+    scanf("%d",&num1);
+    printf("Enter the second number: ");
+    scanf("%d",&num2);
+
+    switch(num1>num2)
+    {
+    case 1:
+        printf("%d is greater",num1);
+        break;
+    case 0:
+    if (num1==num2)
+    {
+        printf("Equal");
+    }
+    else
+    printf("%d is greater",num2);
+    break;
+    
+    default:
+    printf("Invalid Input");
+        break;
+    }
+
+}
