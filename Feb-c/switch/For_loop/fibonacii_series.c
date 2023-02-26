3710e6cd498f80eaf341ec1371cfc3a4538e29f8
pratik kagale <pratikkagale3333@gmail.com>
2023-02-26 23:55:19 +0530

    fibonacci series

--- Feb-c/switch/For_loop/fibonacii_series.c
+++ Feb-c/switch/For_loop/fibonacii_series.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int no,
+    int no,a=0,b=1,c;
+    printf("Enter the number: ");
+    scanf("%d",&no);
+
+    for (int i = 0; i <= no; i++)
+    {
+        c=a+b;
+        printf("%d ",c);
+        a=b;
+        b=c;
+
+    }
+    return 0;
 }
