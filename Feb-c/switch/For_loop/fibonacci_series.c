4a8f137e0c5bc65a2ec6bcc82a0b405625c48e28
pratik <78228798+pratik3333@users.noreply.github.com>
2023-03-01 17:32:13 +0530

    Develop (#19)
    * calculater using switch case in c
    
    * Write a C program to find maximum between two numbers
    using switch case.
    
    * Write a C program to find maximum between two numbers
    using switch case.
    
    * Find Sum of Digits of a number
    
    * write a program to input a number and reverse it
    
    * write a program to input any number and calculate sum of all its digit.
    // 12345=1+2+3+4+5=15
    
    * added space
    
    * fibonacci series
    
    * fibonacci series
    
    * check number is palindrome number or not
    
    * Write a program to print all prime numbers between 1 to n
    
    * pattern printing

--- Feb-c/switch/For_loop/fibonacci_series.c
+++ Feb-c/switch/For_loop/fibonacci_series.c
@@ -1,21 +1,17 @@
 #include<stdio.h>
 int main()
 {
-<<<<<<< HEAD
     int no,a=0,b=1,c;
     printf("Enter the number: ");
     scanf("%d",&no);
 
-    for (int i = 0; i <= no; i++)
+    for (int i=1; i <= no; i++)
     {
         c=a+b;
         printf("%d ",c);
         a=b;
         b=c;
-
     }
     return 0;
-=======
-    int no,
->>>>>>> master
+
 }
---
+++ Feb-c/switch/For_loop/palindrome_number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int no,rem,sum=0,temp;
+
+    printf("Enter the number: ");
+    scanf("%d",&no);
+    temp=no;
+
+
+    while(no>0)
+    {
+        rem=no%10;
+        sum=(sum*10)+rem;
+        no=no/10;
+    }
+    if (temp==sum)
+        printf("Number is palindrome number");
+    else
+        printf("Number is not palindrome number");
+     
+    return 0;
+
+}
