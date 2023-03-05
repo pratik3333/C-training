1981364003b7a737cdd89db0c7c499849032c8fb
pratik kagale <pratikkagale3333@gmail.com>
2023-03-05 20:47:39 +0530

    tribonacci series

---
+++ Feb-c/switch/For_loop/Tribonacci_series.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main()
+{
+    int no,i,a=0,b=1,c=2,d;
+    printf("Enter the term...");
+
+    scanf("%d",&no);
+
+    for ( i = 1; i<=no; i++)
+    {
+        printf(" %d",a);
+        d=a+b+c;
+        a=b;
+        b=c;
+        c=d;
+    }
+    
+}
