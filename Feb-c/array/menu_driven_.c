ad26d0e2ca3a2244db1968e7f91550531219470d
pratik kagale <pratikkagale3333@gmail.com>
2023-03-19 22:45:48 +0530

    menu driven program on array

--- Feb-c/array/menu_driven_.c
+++ Feb-c/array/menu_driven_.c
@@ -1,15 +1,15 @@
 // create menu driven program.
 
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-    int a[10],ch,i,j,no,temp;
+    int a[10], ch, i, j, no, temp, pos;
     printf("Enter the 5 elements of array: \n");
-    for ( i = 0; i < 5; i++)
+    for (i = 0; i < 5; i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d", &a[i]);
     }
-    
+
     printf("\nEnter the 1 for Display elements: ");
     printf("\nEnter the 2 for inserting value at start: ");
     printf("\nEnter the 3 for inserting value at specific index: ");
@@ -21,31 +21,55 @@ void main()
     printf("\nEnter the 9 for delete element at end: ");
 
     printf("\n Enter your choice: \n");
-    scanf("%d",&ch);
+    scanf("%d", &ch);
 
     switch (ch)
     {
     case 1:
         printf("\n Display array: \n");
-        for ( i = 0; i <5; i++)
+        for (i = 0; i < 5; i++)
         {
-            printf("\t%d",a[i]);
+            printf("\t%d", a[i]);
         }
     case 2:
-       printf("\ntype value: \n");
-       scanf("%d",&no);
-    //    a[0]=no;
-       
-       for ( i = 0; i < 6; i--)
-       {
-          temp=a[i];
-          a[i+1]=a[i];
-          a[i]=temp;
-          
-           printf("%d\t",a[i]);
+        printf("\ntype value: \n");
+        scanf("%d", &no);
+        for (i = 6; i > 1; i--)
+        {
+            a[i - 1] = a[i - 2];
+        }
+        a[0] = no;
+        for (i = 0; i < 6; i++)
+        {
+            printf("%d\t", a[i]);
+        }
+
+    case 3:
+        printf("\nEnter position that where you want to: \n");
+        scanf("%d", &pos);
+        printf("\ntype value: \n");
+        scanf("%d", &no);
+        for (i = 5; i >= pos; i--)
+        {
+            a[i] = a[i - 1];
+        }
+        a[pos] = no;
+        for (i = 0; i < 6; i++)
+        {
+            printf("\t%d", a[i]);
+        }
+
+    case 4:
+        printf("\nType the value: \n");
+        scanf("%d", &no);
+
+        a[5] = no;
+        for (i = 0; i < 6; i++)
+        {
+            printf("%d\t", a[i]);
+        }
+
+    case 5:
         
-       }
-       
-    
     }
 }
