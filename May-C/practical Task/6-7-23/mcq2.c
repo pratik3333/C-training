85346a4105a21127d70e79eb7be74a727488d6fe
pratik <78228798+pratik3333@users.noreply.github.com>
2023-07-06 11:50:15 +0530

    Develop (#49)
    * calculater using switch case in c
    
    * Write a C program to find maximum between two numbers
    using switch case.
    
    * Write a C program to find maximum between two numbers
    using switch case.
    
    * Find Sum of Digits of a number
    
    * write a program to input a number and reverse it
    
    * write a program to input any number and calculate sum of all its digit.
    // 12345=1+2+3+4+5=15
    
    * added space
    
    * fibonacci series
    
    * fibonacci series
    
    * check number is palindrome number or not
    
    * Write a program to print all prime numbers between 1 to n
    
    * pattern printing
    
    * pattern printing
    
    * pattern
    
    * character pattern
    
    * numeric pattern
    
    * 55555
    4444
    333
    22
    1
    
    * pattern
    
    * pattern
    
    * pattern printing
    
    * Update pattern7.c
    
    * added ans
    
    * pattern with flag
    
    * pattern
    
    * pattern with %
    
    * c program
    
    * test
    
    * check number is armstrong number or not
    
    * tribonacci series
    
    * tribonacci series
    
    * array arranged in ascending and descending order
    
    * Enter the 5 elements in array and display it
    
    * Find the even and odd numbers in array
    
    * Find the missing element in array
    
    * Find the max number and min number in array
    
    * array program
    
    * array program
    
    * array program
    
    * added space
    
    * WAP to calculate the sum of left and right diagonal element of matrix and create matrix of size 3 x 3
    
    * check the given number is perfect number or not
    
    * array
    
    * WAP to count Digits in a number.
    
    * something went wrong
    
    * traspose of matrix
    
    * sum_of_right_left_diagonal
    
    * multiplication of right diagonal and left diagonal
    
    * something went wrong
    
    * merge two array and sort in descending order
    
    * multiplication of matrix
    
    * write a program to enter the 5 values in ascending order and store in array and find out the missing element from array?
    
    * WAP to enter the 10 element array and print the occurence of every element
    
    * something went wrong
    
    * WAP to seperate odd and even integers in separate arrays?
    
    * array program
    
    * Write a program to create array of size 10 insert elements in it.
    // and display all prime elements from array.
    
    * something going wrong with inserting value at first position
    
    * menu driven program on array
    
    * working
    
    * pattern
    
    * patterns
    
    * patterns
    
    * patterns
    
    * half reverse array
    
    * something went wrong
    
    * c basic
    
    * C files
    
    * checking number is perfect or not
    
    * C program files
    
    * added new pattern
    
    * added new file
    
    * new files
    
    * added chef question
    
    * 22-6-23 practical task
    
    * practical task
    
    * something went wrong with armstrong number
    
    * 27-6-23 practical task
    
    * armstrong numbers up to n
    
    * printing armstrong numbers between 1 to n
    
    * added demo file
    
    * 28-6-23 practical task
    
    * todays practical task
    
    * added some patterns
    
    * todays task
    
    * 5-7-23 practical task
    
    * pattern printing
    
    * added another pattern
    
    * added pattern
    
    * added one more pattern
    
    * sum of first and last digit of given number
    
    * Today's practical task

---
+++ May-C/practical Task/6-7-23/mcq2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+   int i=1;
+   do
+   {
+    printf("%d\n",i);
+    i++;
+    if (i<15)
+    {
+        continue;
+    }
+    
+   } while (0);
+   
+    
+}
---
+++ May-C/practical Task/6-7-23/pattern2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,k;
+    for ( i = 1; i <=5; i++)
+    {
+        k=1;
+        for ( j = 1; j <=9; j++)
+        {
+            if (j>=6-i && j<=4+i)
+            {
+                printf("%d",k);
+                if (j<5)
+                {
+                    k++;
+                }
+                else
+                {
+                    k--;
+                }
+                
+                
+            }
+            else
+            {
+                printf(" ");
+            }
+            
+        }
+        printf("\n");
+    }
+    
+}
+
+//     1    
+//    121
+//   12321
+//  1234321
+// 123454321
