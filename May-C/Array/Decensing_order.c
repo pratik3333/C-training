4a19d32725f8cebbbae14b7e047d7a65863e12bc
pratik kagale <pratikkagale3333@gmail.com>
2023-07-15 11:17:07 +0530

    arranged array in ascending and decending order

--- May-C/Array/Decensing_order.c
+++ May-C/Array/Decensing_order.c
@@ -1,46 +1,42 @@
 #include<stdio.h>
 void main()
 {
-    int a[5],i,temp,len,j;
-
+    int i,a[5],len,temp,j;
     len=sizeof(a)/sizeof(a[0]);
 
-    printf("\nEnter the five values of array:\n");
+    printf("\nEnter the five elements of array: \n");
     for ( i = 0; i <len; i++)
     {
         scanf("%d",&a[i]);
     }
     
-    printf("Display the array Before sorting \n");
+    printf("\nDisplay array before sorting: \n");
     for ( i = 0; i < len; i++)
     {
         printf("\t%d",a[i]);
     }
-
+    
+    //applying sorting logic
     for ( i = 0; i <len; i++)
     {
         for ( j = (i+1); j <len; j++)
         {
-            if (a[i]>a[j])
+            if (a[i]<a[j])
             {
                 temp=a[i];
                 a[i]=a[j];
                 a[j]=temp;
-                
             }
             
         }
         
     }
-    
-    printf("\nDisplay array after sorting: \n");
-    for ( i = 0; i <len; i++)
+
+    printf("\n\nDisplay array after sorting in Decending order: \n");
+    for ( i = 0; i < len; i++)
     {
         printf("\t%d",a[i]);
     }
-    
-
-
     
     
 }
