c609d19050d72a9ecd1fc1eb864a6a3521cdc7dd
pratik kagale <pratikkagale3333@gmail.com>
2023-07-19 11:53:36 +0530

    selection sort

--- May-C/Array/selection_sort.c
+++ May-C/Array/selection_sort.c
@@ -14,7 +14,7 @@ int main()
         printf("%d\t", a[i]);
     }
     // apply selection sort algorithm
-    for (i = 0; i < (len - 1); i++)
+    for (i = 0; i < len; i++)
     {
         min_ind = i;
         for (j = (i + 1); j < len; j++)
