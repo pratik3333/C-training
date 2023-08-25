32c31f7cc80c86683a407cbb6e1a04fc42b93f30
pratik kagale <pratikkagale3333@gmail.com>
2023-08-25 16:30:59 +0530

    Now book will add department wise

--- Library Management Project/ViewBookList.h
+++ Library Management Project/ViewBookList.h
@@ -1,25 +1,74 @@
+
 void viewBookList()
 {
-	int flag=0,count=0,i=0;
-	struct struAddBook b;
-	struct struAddBook *b1;
 	FILE *fptr;
-    
-    fptr=fopen("H:\\C-training\\Library Management Project\\bookDatabase.txt","a+");
-	while(fscanf(fptr,"%d%s%s%d%d%d%d",&b.bookId,&b.bookname,&b.authorName,&b.bookQuantity,&b.bookPrice,&b.bookCount,&b.bookRackno)!=-1)
-    {
-    	count++;
-	}	
-	fclose(fptr);
-	b1=(struct struAddBook*)malloc(sizeof(struct struAddBook)*count);
-    fptr=fopen("H:\\C-training\\Library Management Project\\bookDatabase.txt","a+");
-	while(fscanf(fptr,"%d%s%s%d%d%d%d",&b1[i].bookId,&b1[i].bookname,&b1[i].authorName,&b1[i].bookQuantity,&b1[i].bookPrice,&b1[i].bookCount,&b1[i].bookRackno)!=-1)
-    {
-    	i++;
-	}
-	fclose(fptr);
-	for(i=0;i<count;i++)
+	struct struAddBook b;
+
+	printf("\n\t\t\tComputer Department\n");
+
+	fptr = fopen("H:\\C-training\\Library Management Project\\Computer.txt", "a+");
+
+	while (fscanf(fptr, "%d\t%s\t%s\t%d\t%d\t%d\t%d\n", &b.bookId, &b.bookname, &b.authorName, &b.bookQuantity, &b.bookPrice, &b.bookCount, &b.bookRackno) != EOF)
 	{
-		printf("\n%d\t%s\t%s\t%d\t%d\t%d\t%d\n",b1[i].bookId,b1[i].bookname,b1[i].authorName,b1[i].bookQuantity,b1[i].bookPrice,b1[i].bookCount,b1[i].bookRackno);
+		printf("%d\t%s\t%s\t%d\t%d\t%d\t%d\n", b.bookId, b.bookname, b.authorName, b.bookQuantity, b.bookPrice, b.bookCount, b.bookRackno);
 	}
+	fclose(fptr);
+	// ////////////////////////////////////////////////////////////////////////////////////////
+	printf("\n\t\t\tElectrical Department\n");
+
+	fptr = fopen("H:\\C-training\\Library Management Project\\Electrical.txt", "a+");
+
+	while (fscanf(fptr, "%d\t%s\t%s\t%d\t%d\t%d\t%d\n", &b.bookId, &b.bookname, &b.authorName, &b.bookQuantity, &b.bookPrice, &b.bookCount, &b.bookRackno) != EOF)
+	{
+		printf("%d\t%s\t%s\t%d\t%d\t%d\t%d\n", b.bookId, b.bookname, b.authorName, b.bookQuantity, b.bookPrice, b.bookCount, b.bookRackno);
+	}
+	fclose(fptr);
+
+	// ////////////////////////////////////////////////////////////////////////////////////
+
+	printf("\n\t\t\tCivil Department\n");
+
+	fptr = fopen("H:\\C-training\\Library Management Project\\Civil.txt", "a+");
+
+	while (fscanf(fptr, "%d\t%s\t%s\t%d\t%d\t%d\t%d\n", &b.bookId, &b.bookname, &b.authorName, &b.bookQuantity, &b.bookPrice, &b.bookCount, &b.bookRackno) != EOF)
+	{
+		printf("%d\t%s\t%s\t%d\t%d\t%d\t%d\n", b.bookId, b.bookname, b.authorName, b.bookQuantity, b.bookPrice, b.bookCount, b.bookRackno);
+	}
+	fclose(fptr);
+
+	// ////////////////////////////////////////////////////////////////////////////////////
+
+	printf("\n\t\t\tElectronics Department\n");
+
+	fptr = fopen("H:\\C-training\\Library Management Project\\Electronics.txt", "a+");
+
+	while (fscanf(fptr, "%d\t%s\t%s\t%d\t%d\t%d\t%d\n", &b.bookId, &b.bookname, &b.authorName, &b.bookQuantity, &b.bookPrice, &b.bookCount, &b.bookRackno) != EOF)
+	{
+		printf("%d\t%s\t%s\t%d\t%d\t%d\t%d\n", b.bookId, b.bookname, b.authorName, b.bookQuantity, b.bookPrice, b.bookCount, b.bookRackno);
+	}
+	fclose(fptr);
+
+	// ////////////////////////////////////////////////////////////////////////////////////
+	printf("\n\t\t\tMechanical Department\n");
+
+	fptr = fopen("H:\\C-training\\Library Management Project\\Mechanical.txt", "a+");
+
+	while (fscanf(fptr, "%d\t%s\t%s\t%d\t%d\t%d\t%d\n", &b.bookId, &b.bookname, &b.authorName, &b.bookQuantity, &b.bookPrice, &b.bookCount, &b.bookRackno) != EOF)
+	{
+		printf("%d\t%s\t%s\t%d\t%d\t%d\t%d\n", b.bookId, b.bookname, b.authorName, b.bookQuantity, b.bookPrice, b.bookCount, b.bookRackno);
+	}
+	fclose(fptr);
+
+	// ////////////////////////////////////////////////////////////////////////////////////
+	printf("\n\t\t\tArchitecture Department\n");
+
+	fptr = fopen("H:\\C-training\\Library Management Project\\Architecture.txt", "a+");
+
+	while (fscanf(fptr, "%d\t%s\t%s\t%d\t%d\t%d\t%d\n", &b.bookId, &b.bookname, &b.authorName, &b.bookQuantity, &b.bookPrice, &b.bookCount, &b.bookRackno) != EOF)
+	{
+		printf("%d\t%s\t%s\t%d\t%d\t%d\t%d\n", b.bookId, b.bookname, b.authorName, b.bookQuantity, b.bookPrice, b.bookCount, b.bookRackno);
+	}
+	fclose(fptr);
+
+	// ////////////////////////////////////////////////////////////////////////////////////
 }
